fff73d73aac24f358eb8235ca7585a8162d9eb90
Mukul <mukul.devry99@gmail.com>
2024-04-09 23:00:29 +0530

    come check this....

--- src/main/java/com/driver/repository/DriverRepository.java
+++ src/main/java/com/driver/repository/DriverRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.driver.model.Driver;
+
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Integer>{
 
